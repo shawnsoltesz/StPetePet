1a15ba2d224185613c72f6f4a26248a47d807839
Shawn Soltesz <shawnjsoltesz@gmail.com>
2021-09-21 15:52:47 -0400

    header and footer HTML/CSS

--- ClientApp/src/App.tsx
+++ ClientApp/src/App.tsx
@@ -1,6 +1,27 @@
 import React from 'react'
-//import logo from '.images/logo/stpeteDraft.png'
+// import logo from '.images/logo/stpetepetDraft.png'
 
 export function App() {
-  return <div>Hello, World</div>
+  return (
+    <>
+      <header>
+        {/* <img
+          src={logo}
+          alt="St Pete Pet logo of dog and cat at the beach."
+          height="64"
+          width="64"
+        /> */}
+
+        <h1>StPete.Pet</h1>
+      </header>
+      <footer>
+        <p>
+          Built with <i className="fas fa-paw"></i> in St. Petersburg, Florida.
+        </p>
+        {/* <p>
+          Built with <i className="fa fa-heart"></i> in St Petersburg, Florida.
+        </p> */}
+      </footer>
+    </>
+  )
 }
