4f49c72cfb6195aea756b1428e72f05b9dc65462
Shawn Soltesz <shawnjsoltesz@gmail.com>
2021-09-22 15:26:52 -0400

    New Account Page

--- ClientApp/src/App.tsx
+++ ClientApp/src/App.tsx
@@ -2,7 +2,8 @@ import React from 'react'
 // import { Home } from './Pages/Home'
 // import { ListingDetail } from './Pages/ListingDetail'
 // import { About } from './Pages/About'
-import { Login } from './Pages/Login'
+// import { Login } from './Pages/Login'
+import { NewAccount } from './Pages/NewAccount'
 import logo from './images/stpetepet/SPPlogo.svg'
 
 export function App() {
@@ -13,7 +14,7 @@ export function App() {
           <img src={logo} alt="St Pete Pet logo of dog and cat at the beach." />
         </div>
       </header>
-      <Login />
+      <NewAccount />
       <footer>
         <p>
           Built with <i className="heart fa fa-heart"></i> in St Petersburg,
