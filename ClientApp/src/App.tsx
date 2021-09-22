0d380b958e0326ff7fb982bebb04605174c0795f
Shawn Soltesz <shawnjsoltesz@gmail.com>
2021-09-21 20:55:41 -0400

    added search placeholder, removed Hamburger Menu and added map placeholder

--- ClientApp/src/App.tsx
+++ ClientApp/src/App.tsx
@@ -1,30 +1,33 @@
 import React from 'react'
-// import HamburgerMenu from './components/HamburgerMenu'
+
 // import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 // import Home from "./components/Pages/Home";
 // import About from "./components/Pages/About";
-//import logo from '.images/logo/stpetepetDraft.png'
+import logo from './images/logo/stpetepetDraft.png'
+import stpete from './images/map/map-stpete-fl.gif'
 
 export function App() {
   return (
     <>
       <header>
-        {/* <img
-          src={logo}
-          alt="St Pete Pet logo of dog and cat at the beach."
-          height="64"
-          width="64"
-        /> */}
-
-        <h1>StPete.Pet</h1>
+        <div className="logo">
+          <img src={logo} alt="St Pete Pet logo of dog and cat at the beach." />
+        </div>
       </header>
+      <main>
+        <div className="search">
+          <input type="text" placeholder="Search..." />
+        </div>
+
+        <div className="map">
+          <img src={stpete} alt="aerial map of St Petersburg, FL" />
+        </div>
+      </main>
       <footer>
         <p>
-          Built with <i className="fas fa-paw"></i> in St. Petersburg, Florida.
+          Built with <i className="heart fa fa-heart"></i> in St Petersburg,
+          Florida. <i className="paw fas fa-paw"></i>
         </p>
-        {/* <p>
-          Built with <i className="fa fa-heart"></i> in St Petersburg, Florida.
-        </p> */}
       </footer>
     </>
   )
