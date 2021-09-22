734f4cc766ebdc0dc2f3b92a4444d0aec7f5957b
Shawn Soltesz <shawnjsoltesz@gmail.com>
2021-09-21 22:18:15 -0400

    experimenting with CSS

--- ClientApp/src/App.tsx
+++ ClientApp/src/App.tsx
@@ -14,15 +14,47 @@ export function App() {
           <img src={logo} alt="St Pete Pet logo of dog and cat at the beach." />
         </div>
       </header>
-      <main>
-        <div className="search">
-          <input type="text" placeholder="Search..." />
-        </div>
 
-        <div className="map">
-          <img src={stpete} alt="aerial map of St Petersburg, FL" />
-        </div>
-      </main>
+      <div className="search">
+        <input type="text" placeholder="Search..." />
+      </div>
+
+      <div className="mapimage">
+        <img src={stpete} alt="aerial map of St Petersburg, FL" />
+      </div>
+
+      <div>
+        <ul className="listing1">
+          <li>
+            <h2>Name</h2>
+          </li>
+          <li>
+            <p>Address</p>
+          </li>
+        </ul>
+      </div>
+
+      <div>
+        <ul className="listing2">
+          <li>
+            <h2>Name</h2>
+          </li>
+          <li>
+            <p>Address</p>
+          </li>
+        </ul>
+      </div>
+
+      <div>
+        <ul className="listing3">
+          <li>
+            <h2>Name</h2>
+          </li>
+          <li>
+            <p>Address</p>
+          </li>
+        </ul>
+      </div>
       <footer>
         <p>
           Built with <i className="heart fa fa-heart"></i> in St Petersburg,
