6f905bc49a0a50337ae023f77b03969987164e63
Shawn Soltesz <shawnjsoltesz@gmail.com>
2021-09-23 13:36:50 -0400

    Listing db migration

---
+++ Migrations/20210923173341_CreateListing.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+namespace StPetePet.Migrations
+{
+    public partial class CreateListing : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Listings",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    IsActive = table.Column<bool>(type: "boolean", nullable: false),
+                    ListingType = table.Column<string>(type: "text", nullable: true),
+                    Name = table.Column<string>(type: "text", nullable: true),
+                    Description = table.Column<string>(type: "text", nullable: true),
+                    Address = table.Column<string>(type: "text", nullable: true),
+                    Website = table.Column<string>(type: "text", nullable: true),
+                    PhoneNumber = table.Column<string>(type: "text", nullable: true),
+                    CreatedDate = table.Column<DateTime>(type: "timestamp without time zone", nullable: false),
+                    UpdatedDate = table.Column<DateTime>(type: "timestamp without time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Listings", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Listings");
+        }
+    }
+}
--- Models/Listing.cs
+++ Models/Listing.cs
@@ -1,15 +1,17 @@
+using System;
+
 namespace StPetePet.Models
 {
     public class Listing
     {
         public int Id {get; set;}
-        public boolean IsActive {get; set;}
+        public bool IsActive {get; set;}
         public string ListingType {get; set;}
         public string Name {get; set;}
         public string Description {get; set;}
         public string Address {get; set;}
         public string Website {get; set;}
-        public varchar12 PhoneNumber {get; set;}
+        public string PhoneNumber {get; set;}
         public DateTime CreatedDate {get; set;}
         public DateTime UpdatedDate {get; set;}
     }
