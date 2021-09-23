df5f8f239f7c03bd76a3f2a3a034361ce829c5a7
Shawn Soltesz <shawnjsoltesz@gmail.com>
2021-09-23 12:35:03 -0400

    added types page and adjusted footer css

--- ClientApp/src/Pages/Home.tsx
+++ ClientApp/src/Pages/Home.tsx
@@ -8,6 +8,26 @@ export function Home() {
       <div className="search">
         <input type="text" placeholder="Search..." />
       </div>
+      <div className="search-dropdown">
+        <p>Listing Type: &nbsp; &nbsp; </p>
+
+        <select name="listing-specialty" id="listing-specialty">
+          <option value="null">Select</option>
+          <option value="bar-restaurant">Bar &amp; Restaurant </option>
+          <option value="beach">Beach</option>
+          <option value="boarding">Boarding</option>
+          <option value="event">Event</option>
+          <option value="grooming">Grooming</option>
+          <option value="lodging">Lodging</option>
+          <option value="medical-care">Medical Care</option>
+          <option value="park">Park</option>
+          <option value="pet-sitter">Pet Sitter</option>
+          <option value="residential">Residential</option>
+          <option value="specialty">Specialty</option>
+          <option value="supplies">Supplies &amp; Care</option>
+          <option value="transportation">Transportation</option>
+        </select>
+      </div>
       <span className="map">
         <div className="map-image">
           <img src={StPete} alt="aerial map of St Petersburg, FL" />
