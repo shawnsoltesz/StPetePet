6e0c6f495a6503919ec3203695e01bd943266166
Shawn Soltesz <shawnjsoltesz@gmail.com>
2021-09-25 18:00:01 -0400

    update to listing component to update listing name link

--- ClientApp/src/components/Pages/SingleListingFromList.tsx
+++ ClientApp/src/components/Pages/SingleListingFromList.tsx
@@ -1,10 +1,14 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 import { ListingType } from '../../types'
 
 export function SingleListingFromList(props: SingleListingFromListProps) {
+  const urlForShowingListing = `/listings/${props.listing.id}`
   return (
     <li>
-      <h2>{props.listing.name}</h2>
+      <h2>
+        <Link to={urlForShowingListing}>{props.listing.name}</Link>
+      </h2>
       <p>Type: {props.listing.listingType}</p>
       <p>{props.listing.address}</p>
     </li>
