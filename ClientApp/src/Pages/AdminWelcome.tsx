17d3c1c5f6f153d6ca3e9ff3276b1ffe3d700751
Shawn Soltesz <shawnjsoltesz@gmail.com>
2021-09-27 16:23:47 -0400

    experimenting with footer CSS and admin page layout

--- ClientApp/src/Pages/AdminWelcome.tsx
+++ ClientApp/src/Pages/AdminWelcome.tsx
@@ -4,14 +4,13 @@ import { Link } from 'react-router-dom'
 export function AdminWelcome() {
   return (
     <>
-      <button className="logout">Logout</button>
-
-      <div className="admin-page">
+      <div className="admin-nav">
+        <button className="logout">Logout</button>
         <div className="breadcrumb" aria-label="breadcrumbs">
           <ul>
             <li>
               <p>
-                <i className="breadcrumb-icon fas fa-home"></i>{' '}
+                <i className="breadcrumb-icon fas fa-home"></i>
                 <Link to="/">Home</Link> &nbsp;/&nbsp;
               </p>
             </li>
@@ -23,24 +22,31 @@ export function AdminWelcome() {
             </li>
           </ul>
         </div>
+      </div>
+      <div className="admin-page">
         <h1 className="listing-name">StPete.Pet Admin</h1>
 
         <div className="search-listing">
           <div className="admin-menu">
             <h2>Listings</h2>
 
-            <button className="listing-menu">Create Listing</button>
+            <Link className="listing-menu" to="/admin/newlisting">
+              <button>Create Listing</button>
+            </Link>
 
             <br />
             <br />
+            <Link className="listing-menu" to="/admin/updatelisting">
+              <button>Update / Delete Listing</button>
+            </Link>
 
-            <button className="listing-menu">Update / Delete Listing</button>
             <h2>Users</h2>
 
             <br />
             <br />
-
-            <button className="user-menu">Create / Update User</button>
+            <Link className="listing-menu" to="/admin/updateaccount">
+              <button>Update / Delete Listing</button>
+            </Link>
 
             <br />
             <br />
