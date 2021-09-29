4b62b08f2f4ec886e4d5ddb436289fb066ad00b7
Shawn Soltesz <shawnjsoltesz@gmail.com>
2021-09-28 20:39:43 -0400

    tweaks to styling of pages and added logout button

--- ClientApp/src/Pages/About.tsx
+++ ClientApp/src/Pages/About.tsx
@@ -8,11 +8,9 @@ export function About() {
       <div className="breadcrumb" aria-label="breadcrumbs">
         <ul>
           <li>
-            <p>
-              <i className="breadcrumb-icon fas fa-home"></i>{' '}
-              <Link to="/">Home</Link>
-              &nbsp;/
-            </p>
+            <i className="breadcrumb-icon fas fa-home"></i>
+            <Link to="/">Home</Link>
+            &nbsp;/
           </li>
           <li className="is-active">
             <p aria-current="page">
@@ -22,25 +20,28 @@ export function About() {
           </li>
         </ul>
       </div>
-      <h1 className="listing-name">About</h1>
-      <div className="about">
-        <div className="seal">
-          <img
-            src={seal}
-            alt="St Pete dot Pet circular seal containing an image of a dog and cat at the beach. Establish 2021"
-          />
-        </div>
-        <div className="description">
-          <p>
-            <strong>StPete&#46;Pet</strong> is a website that celebrates St&#46;
-            Petersburg&#44; FL as a city comprised of numerous local businesses
-            and points of interest that welcome pets and their humans&#46;
+      <body>
+        <h1 className="listing-name">About</h1>
+        <div className="about">
+          <div className="seal">
+            <img
+              src={seal}
+              alt="St Pete dot Pet circular seal containing an image of a dog and cat at the beach. Establish 2021"
+            />
+          </div>
+          <div className="description">
             <p>
-              <strong>#PetsWelcomeHere</strong>
+              <strong>StPete&#46;Pet</strong> is a website that celebrates
+              St&#46; Petersburg&#44; FL as a city comprised of numerous local
+              businesses and points of interest that welcome pets and their
+              humans&#46;
+              <p>
+                <strong>#PetsWelcomeHere</strong>
+              </p>
             </p>
-          </p>
+          </div>
         </div>
-      </div>
+      </body>
     </>
   )
 }
--- ClientApp/src/Pages/AdminWelcome.tsx
+++ ClientApp/src/Pages/AdminWelcome.tsx
@@ -4,60 +4,64 @@ import { Link } from 'react-router-dom'
 export function AdminWelcome() {
   return (
     <>
-      <div className="admin-nav">
-        <button className="logout">Logout</button>
-        <div className="breadcrumb" aria-label="breadcrumbs">
-          <ul>
-            <li>
-              <p>
-                <i className="breadcrumb-icon fas fa-home"></i>
-                <Link to="/">Home</Link> &nbsp;/&nbsp;
-              </p>
-            </li>
-            <li className="is-active">
-              <p aria-current="page">
-                <i className="breadcrumb-icon fas fa-paw"></i>&nbsp;StPete.Pet
-                Admin
-              </p>
-            </li>
-          </ul>
-        </div>
-      </div>
-      <div className="admin-page">
-        <h1 className="listing-name">StPete.Pet Admin</h1>
+      <main>
+        <nav>
+          <div className="admin-nav">
+            <button className="logout">Logout</button>
+            <div className="breadcrumb" aria-label="breadcrumbs">
+              <ul>
+                <li>
+                  <p>
+                    <i className="breadcrumb-icon fas fa-home"></i>
+                    <Link to="/">Home</Link> &nbsp;/&nbsp;
+                  </p>
+                </li>
+                <li className="is-active">
+                  <p aria-current="page">
+                    <i className="breadcrumb-icon fas fa-paw"></i>
+                    &nbsp;StPete.Pet Admin
+                  </p>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </nav>
+        <div className="admin-page">
+          <h1 className="listing-name">StPete.Pet Admin</h1>
 
-        <div className="search-listing">
-          <div className="admin-menu">
-            <h2>Listings</h2>
+          <div className="search-listing">
+            <div className="admin-menu">
+              <h2>Listings</h2>
 
-            <Link className="listing-menu" to="/admin/addlisting">
-              <button>Add Listing</button>
-            </Link>
+              <Link className="listing-menu" to="/admin/addlisting">
+                <button>Add Listing</button>
+              </Link>
 
-            <br />
-            <br />
-            <Link className="listing-menu" to="/admin/updatelisting">
-              <button>Update / Delete Listing</button>
-            </Link>
+              <br />
+              <br />
+              <Link className="listing-menu" to="/admin/updatelisting">
+                <button>Update / Delete Listing</button>
+              </Link>
 
-            <h2>Users</h2>
+              <h2>Users</h2>
 
-            <br />
-            <br />
-            <Link className="listing-menu" to="/admin/adduser">
-              <button>Add Users</button>
-            </Link>
-            <br />
-            <br />
-            <Link className="listing-menu" to="/admin/updateuser">
-              <button>Update / Delete Users</button>
-            </Link>
+              <br />
+              <br />
+              <Link className="listing-menu" to="/admin/adduser">
+                <button>Add Users</button>
+              </Link>
+              <br />
+              <br />
+              <Link className="listing-menu" to="/admin/updateuser">
+                <button>Update / Delete Users</button>
+              </Link>
 
-            <br />
-            <br />
+              <br />
+              <br />
+            </div>
           </div>
         </div>
-      </div>
+      </main>
     </>
   )
 }
