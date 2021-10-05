936bf226f3d2b32605663c95c596476b875f21a1
Shawn Soltesz <shawnjsoltesz@gmail.com>
2021-10-04 21:29:11 -0400

    added Controllers and migration for photoURL

--- Models/Listing.cs
+++ Models/Listing.cs
@@ -5,32 +5,34 @@ namespace StPetePet.Models
 {
     public class Listing
     {
-        public int Id {get; set;}
-        public bool IsActive {get; set;}
-        
+        public int Id { get; set; }
+        public bool IsActive { get; set; }
+
         [Required(ErrorMessage = "You must select a Listing Type.")]
-        public string ListingType {get; set;}
-        
+        public string ListingType { get; set; }
+
         [Required(ErrorMessage = "You must enter a Name.")]
-        public string Name {get; set;}
-        
+        public string Name { get; set; }
+
         [Required(ErrorMessage = "You must enter a Description.")]
-        public string Description {get; set;}
-        
+        public string Description { get; set; }
+
         [Required(ErrorMessage = "You must enter an Address.")]
-        public string Address {get; set;}
+        public string Address { get; set; }
 
         public double Latitude { get; set; }
 
         public double Longitude { get; set; }
-        
+
         [Required(ErrorMessage = "You must enter a Website.")]
-        public string Website {get; set;}
-        
+        public string Website { get; set; }
+
         [Required(ErrorMessage = "You must enter a Telephone Number.")]
-        public string PhoneNumber {get; set;}
-        
-        public DateTime CreatedDate {get; set;}
-        public DateTime UpdatedDate {get; set;}
+        public string PhoneNumber { get; set; }
+
+        public DateTime CreatedDate { get; set; }
+        public DateTime UpdatedDate { get; set; }
+
+        public string PhotoURL { get; set; }
     }
 }
